307df0d0460026df1b904102d060b53cab724eab
Fayth7 <flexflakes@gmail.com>
2023-11-30 19:38:56 +0000

    correction for task 1 & 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,4 +1,15 @@
+/**
+ * Represents a Holberton Course.
+ */
+
 export default class HolbertonCourse {
+      /**
+   * Creates a new @see {@link HolbertonCourse}.
+   *
+   * @param {String} name - The name of the course.
+   * @param {Number} length - How long the course is (in months).
+   * @param {String[]} students - The names of students in the course.
+   */
   constructor(name, length, students) {
     this._validateString(name, 'Name');
     this._name = name;
