85791c688847cb8939ccc156c35b7eda6dc94482
Fayth7 <flexflakes@gmail.com>
2023-11-30 19:18:56 +0000

    corrections to tasks

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,40 +1,50 @@
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._validateCurrency(currency);
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(newAmount) {
-    this._amount = newAmount;
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = value;
   }
 
+  /**
+   * @returns {Currency}
+   */
   get currency() {
     return this._currency;
   }
 
-  set currency(newCurrency) {
-    this._validateCurrency(newCurrency);
-    this._currency = newCurrency;
+  /**
+   * @param {Currency} value
+   */
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = value;
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
-  }
-
-  _validateCurrency(currency) {
-    if (!(currency instanceof Currency)) {
-      throw new TypeError('Currency must be an instance of the Currency class');
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
     }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
+    return amount * conversionRate;
   }
 }
