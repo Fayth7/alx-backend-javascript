6981d7c59a7c2a00913007129d689f06c851c8a7
Fayth7 <flexflakes@gmail.com>
2024-01-30 08:47:15 -0800

    updates task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,33 +1,35 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
-
+const students = require('./3-read_file_async');
 const app = express();
+const hostname = '127.0.0.1';
 const port = 1245;
 
-/**
- * Counts the students in a CSV data file.
- * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-    // Assuming the database filename is passed as a command line argument
-    const [, , databaseFileName] = process.argv;
+app.get('/students', async (req, res) => {
+  try {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('This is the list of our students\n');
 
-    countStudents(databaseFileName)
-        .then(() => {
-            res.end();
-        })
-        .catch((error) => {
-            res.end(error.message);
-        });
+    const data = await students(process.argv[2]);
+
+    res.write(`Number of students: ${data.students.length}\n`);
+    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+  } catch (err) {
+    res.write(err.message);
+  } finally {
+    res.end();
+  }
 });
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
 
 module.exports = app;
