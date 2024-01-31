c4ea0f4e446fff374c6190cefe936a3e8ca6817b
Fayth7 <flexflakes@gmail.com>
2024-01-31 08:10:45 -0800

    task 4

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,15 +1,20 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', function () {
-  it('should call Utils.calculateNumber with correct arguments', function () {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
 
+describe('sendPaymentRequestToApi function', () => {
+  // create the spy / wrapper of an existing function
+  const utilSpy = sinon.spy(Utils, 'calculateNumber');
+
+  it('validate the usage of the Utils function', () => {
+    // run the function with the parameters
     sendPaymentRequestToApi(100, 20);
-
-    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
-
-    calculateNumberSpy.restore();
+    // check that the spy was called exactly once
+    expect(utilSpy.calledOnce).to.be.true;
+    // check that spy was called at least once with the provided arguments
+    expect(utilSpy.calledWith('SUM', 100, 20)).to.be.true;
+    utilSpy.restore()
   });
+});
