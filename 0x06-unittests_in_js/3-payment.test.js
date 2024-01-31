3a60532e1c3857e67f06a3d29e1c61860934d2e5
Fayth7 <flexflakes@gmail.com>
2024-01-31 08:03:40 -0800

    task 3

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function () {
+  it('should call Utils.calculateNumber with correct arguments', function () {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+
+    calculateNumberSpy.restore();
+  });
